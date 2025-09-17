6b6c328f2a8769aca672eb1aecf7de3b5000bb08
AYNSJC <chandhokaarya@gmail.com>
2025-09-17 21:19:46 +0530

    clear screen support

--- cmd-plus-plus.py
+++ cmd-plus-plus.py
@@ -9,6 +9,16 @@ border = "    | "
 current_directory = os.getcwd()
 print(f"{border}Current working directory: {current_directory}")
 
+def start_cmd():
+    global running
+    running = True
+
+    global current_line
+    current_line = 0
+
+    current_directory = os.getcwd()
+    print(f"{border}Current working directory: {current_directory}")
+
 def change_directory(command:list[str], token_no:int):
     os.chdir(command[token_no + 1])
     current_directory = os.getcwd()
@@ -40,6 +50,7 @@ def print_sequence(sequence:list[str], showing_files:bool):
         for j in range(0, len(file_display_sequence), 1):
             print(f"{border}{file_display_sequence[j]}")
 
+
 while running:
     current_command = input(f"{current_line:<2}. | >")
     current_line += 1
@@ -52,6 +63,10 @@ while running:
 
         if(tokens[i] == "ls" or tokens[i] == "show"):
             list_directory_contents(tokens, i)
+
+        if(tokens[i] == "cls" or tokens[i] == "clear"):
+            os.system("cls")
+            start_cmd()
 
         if(tokens[i] == "quit"):
             running = False
