f8157fa46451f9d98edf4a9e66fa71bd24a83dde
AYNSJC <chandhokaarya@gmail.com>
2025-09-17 18:41:52 +0530

    line counter and list files function

--- cmd-plus-plus.py
+++ cmd-plus-plus.py
@@ -2,6 +2,8 @@ import os
 
 running = True
 
+current_line = 0
+
 current_directory = os.getcwd()
 print(f"Current working directory: {current_directory}")
 
@@ -10,14 +12,30 @@ def change_directory(command:list[str], token_no:int):
     current_directory = os.getcwd()
     print(f"{current_directory}")
 
+def list_directory_contents(command:list[str], token_no:int):
+    if(token_no + 1 < len(command)):
+        files = os.listdir(command[token_no + 1])
+        print_sequence(files)
+    else:
+        files = os.listdir(os.getcwd())
+        print_sequence(files)
+
+def print_sequence(sequence:list[str]):
+    for i in range(0, len(sequence), 1):
+        print(sequence[i])
+
 while running:
-    current_command = input(">")
+    current_command = input(f"{current_line:<2}. | >")
+    current_line += 1
 
     tokens = current_command.split(" ")
 
     for i in range(0, len(tokens), 1):
         if(tokens[i] == "cd"):
             change_directory(tokens, i)
+
+        if(tokens[i] == "ls"):
+            list_directory_contents(tokens, i)
 
         if(tokens[i] == "quit"):
             running = False
